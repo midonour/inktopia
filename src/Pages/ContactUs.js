f48659369f31042fa5d6282186d70eeb37757198
midonour <mnw2637@gmail.com>
2025-12-08 02:28:29 +0200

    handle sending emails

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,15 +1,33 @@
-import React,{useRef} from "react";
+import React, { useRef } from "react";
 import "../Styles/ContactUs.css";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 function ContactUs() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_iuufj3s", "template_rfg3hbi", form.current, {
+        publicKey: "qBPGm0sdOLrFBOm0-",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <div className="contact-container">
-      <form className="contact-form">
+      <form className="contact-form" ref={form} onSubmit={sendEmail}>
         <h1 className="contact-title">Contact Us</h1>
 
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Your Message" rows={5}></textarea>
+        <input type="text" placeholder="Name" name="name"/>
+        <input type="email" placeholder="Email" name="email"/>
+        <textarea placeholder="Your Message" rows={5} name="message"></textarea>
 
         <button type="submit" className="contact-btn">
           Send Message
